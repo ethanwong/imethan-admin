ae2e7e8c22c46c418da8523578dd0e4495338e16
ethanwong@qq.com <ethanwong@qq.com>
2015-08-29 09:50:29 +0800

    first commit

---
+++ src/main/java/cn/imethan/admin/web/console/IndexControl.java
@@ -0,0 +1,25 @@
+package cn.imethan.admin.web.console;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * IndexControl.java
+ *
+ * @author Ethan Wong
+ * @time 2015年8月28日下午10:45:50
+ */
+@Controller
+@RequestMapping("/console")
+public class IndexControl {
+	
+    @RequestMapping("")
+    public String indexOne(Model model) {
+    	System.out.println("----------------");
+    	return "console/index";
+    }
+
+}
+
+
